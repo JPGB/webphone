22df8337445dc01639e21e4676ca329bb1e76872
João Galindo <joao.galindo@l5.com.br>
2022-04-11 18:02:17 -0300

    initializing message listener

--- components/ContextProvider/index.js
+++ components/ContextProvider/index.js
@@ -1,8 +1,38 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import { Logger } from 'sip.js/lib/core';
 
 export const GlobalContext = createContext();
 
 const ContextProvider = ({ children }) => {
+
+    useEffect(() => {
+        window.addEventListener("message", (e) => {
+            if (e.data?.event) {
+                let { event, parameters } = e.data
+                switch (event) {
+                    case 'handleChannelState':
+                        setChannelState(parameters)
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
+    }, [])
+
+    // useEffect(() => {
+    //     const handler = event => {
+    //         const data = event.data
+    //         console.log("Hello World?", data)
+    //     }
+
+    //     window.addEventListener("message", handler)
+
+    //     // clean up
+    //     return () => window.removeEventListener("message", handler)
+    // }, []) // empty array => run only once
+
     const [channelState, setChannelState] = useState([
         { active: true, free: true, line: "1", number: "9009", state: "setup", mute: false, hold: false },
         { active: false, free: true, line: "2", number: "9008", state: "setup", mute: false, hold: false },
